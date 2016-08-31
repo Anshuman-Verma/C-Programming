aaa0212fcb3481fa4d6e542bb964065e25f4fdc2
Natsu7 <anshu.av97@gmail.com>
2016-08-31 23:54:19 +0530

    Add files via upload
    Take input of matrix elements and print it as a matrix

---
+++ Matrix_Print.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+int row,col,i,j;
+printf("Input Matrix Rows\n");
+scanf("%d",&row);
+
+printf("Input Matrix Column\n");
+scanf("%d",&col);
+
+int M[row][col];
+
+printf("Input Matrix Elements\n");
+
+for( i=0; i<row; i++)
+	{ 
+		for( j=0; j<col; j++){
+		scanf("%d",&M[i][j]);	
+		}
+	}
+printf("The Matrix Obtained is:\n");
+for(i=0; i<row; i++)
+	{ 
+		for(j=0; j<col; j++){
+		printf("%d",M[i][j]);
+		printf(" ");
+		}	
+	printf("\n");
+	}
+return 0;
+}
