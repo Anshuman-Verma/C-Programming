8913b8583b2a08f61371b2f7577c01a1c548ea05
Piyush Pawar <piyushpawar25@gmail.com>
2018-02-03 13:57:50 +0530

    Update Array_Element_Frequency.c

--- Array_Element_Frequency.c
+++ Array_Element_Frequency.c
@@ -1,36 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
+int main() {
 
-int n;
-printf("Enter Array Length: ");
-scanf("%d",&n);
+	int n;
+	printf("Enter Array Length: ");
+	scanf("%d", &n);
 
-printf("Enter Array Elements");
-int A[n],i,j,temp[n];
-for(i=0;i<n;i++)
-	{
-	scanf("%d",&A[i]);
-	temp[i]=A[i];
+	printf("Enter Array Elements");
+	int A[n], i, j, temp[n];
+	for(i=0;i<n;i++) {
+		scanf("%d", &A[i]);
+		temp[i] = A[i];
 	}
 
-int count=0;
+	int count = 0;
 
-for(i=0;i<n;i++)
-	{ count =0;
-	if(temp[i]!=0)
-	  {	for(j=i;j<n;j++)
-		 {	
-			{
-			if(A[i]==A[j])
-				{
-				count++;
-				temp[j]=0;
+	for(i = 0; i < n; i++) { 
+		count = 0;
+		if(temp[i] != 0) {	
+			for(j = i; j < n; j++) {	
+				if(A[i] == A[j]) {
+					count++;
+					temp[j] = 0;
 				}
 			}
-		}
-printf("Frequency of %d is %d\n",A[i],count);
-	}  }
-return 0;
+		printf("Frequency of %d is %d\n",A[i],count);
+		}  
+	}
+	return 0;
 }
